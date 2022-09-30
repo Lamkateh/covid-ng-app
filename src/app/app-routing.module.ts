9fbd4c7fddbc9583ce753a6a9c48c0dcc1e3d1fc
Lamkateh <bruno.dilivio@gmail.com>
2022-09-30 17:54:54 +0200

    Add button to list item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePublicPageComponent } from './home-public-page/home-public-page.component';
-import { VaccinationCenterListComponent } from './vaccination-center-list/vaccination-center-list.component';
 import { VaccinationCenterComponent } from './vaccination-center/vaccination-center.component';
 
 const routes: Routes = [
   { path: 'centers', component: HomePublicPageComponent },
-  { path: 'centers/detail/:id', component: VaccinationCenterComponent },
+  { path: 'centers/:id', component: VaccinationCenterComponent },
   { path: '', redirectTo: '/centers', pathMatch: 'full' },
 ];
 

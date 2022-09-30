be9a7415ee05e8412c8e99214eef17e721e794dd
Lamkateh <bruno.dilivio@gmail.com>
2022-09-30 15:10:16 +0200

    Add api call vaccination centers

--- src/app/vaccination-center-list/vaccination-center-list.component.ts
+++ src/app/vaccination-center-list/vaccination-center-list.component.ts
@@ -1,42 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { VaccinationCenterService } from '../services/vaccination-center.service';
 import { VaccinationCenter } from '../vaccination-center/vaccination-center';
 
 @Component({
   selector: 'app-vaccination-center-list',
   templateUrl: './vaccination-center-list.component.html',
-  styleUrls: ['./vaccination-center-list.component.scss']
+  styleUrls: ['./vaccination-center-list.component.scss'],
 })
 export class VaccinationCenterListComponent implements OnInit {
+  centers?: VaccinationCenter[] = [
+    {
+      id: 1,
+      name: 'Hopital Central',
+      address: '140 rue Saint Dizier',
+      zipCode: '54000',
+      city: 'Nancy',
+      phone: '0345567889',
+      email: 'hopitalcentral@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'Hopital Nord',
+      address: '140 rue de Metz',
+      zipCode: '54000',
+      city: 'Nancy',
+      phone: '0345567889',
+      email: 'hopitalnord@gmail.com',
+    },
+    {
+      id: 3,
+      name: 'Hopital Ouest',
+      address: "140 rue Jeanne d'Arc",
+      zipCode: '54000',
+      city: 'Nancy',
+      phone: '0345567889',
+      email: 'hopitalouest@gmail.com',
+    },
+  ];
 
-  centers?: VaccinationCenter[] = [{
-    id: 1,
-    name: "Hopital Central",
-    address: "140 rue Saint Dizier",
-    zipCode: "54000",
-    city: "Nancy",
-    phone: "0345567889",
-    email: "hopitalcentral@gmail.com"
-  }, {
-    id: 2,
-    name: "Hopital Nord",
-    address: "140 rue de Metz",
-    zipCode: "54000",
-    city: "Nancy",
-    phone: "0345567889",
-    email: "hopitalnord@gmail.com"
-  }, {
-    id: 3,
-    name: "Hopital Ouest",
-    address: "140 rue Jeanne d'Arc",
-    zipCode: "54000",
-    city: "Nancy",
-    phone: "0345567889",
-    email: "hopitalouest@gmail.com"
-  }];
-
-  constructor() { }
+  constructor(private service: VaccinationCenterService) {}
 
   ngOnInit(): void {
+    this.service.getVaccinationCentersByCity('Nancy').subscribe((centers) => {
+      this.centers = centers;
+    });
   }
-
 }

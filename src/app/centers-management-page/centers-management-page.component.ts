2061c2327aebb24f8fdcb32014040c7afbd9db33
Gaëtan NOUSSE <gaetannousse@gmail.com>
2022-10-06 18:34:22 +0200

    Create centers management page

--- src/app/centers-management-page/centers-management-page.component.ts
+++ src/app/centers-management-page/centers-management-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { VaccinationCenterService } from '../services/vaccination-center.service';
+import { VaccinationCenter } from '../vaccination-center/vaccination-center';
+
 
 @Component({
   selector: 'app-centers-management-page',
@@ -6,10 +9,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./centers-management-page.component.scss']
 })
 export class CentersManagementPageComponent implements OnInit {
+  centers?: VaccinationCenter[] = [];
+  citySearchTerm: string = '';
+  citySearched: string = '';
+  listLoading: boolean = false;
+  page: number = 0;
 
-  constructor() { }
+  onSearchCity() {
+    this.citySearched = this.citySearchTerm;
+    this.page = 0;
+    this.getResult();
+  }
+
+  onMoreRequest() {
+    this.page++;
+    this.getResult();
+  }
+
+  getResult() {
+    this.listLoading = true;
+    if (this.page === 0) {
+      this.centers = [];
+    }
+    if (this.citySearched === '') {
+      this.service
+        .getAllVaccinationCenters(this.page)
+        .subscribe((centers: { content: VaccinationCenter[] }) => {
+          this.centers.push(...centers.content);
+          this.listLoading = false;
+        });
+    } else {
+      this.service
+        .getVaccinationCentersByCity(this.citySearched, this.page)
+        .subscribe((centers: { content: VaccinationCenter[] }) => {
+          this.centers.push(...centers.content);
+          this.listLoading = false;
+        });
+    }
+  }
+
+  isLoading() {
+    if (this.listLoading) return true;
+    else return false;
+  }
+
+  isNotLoading() {
+    if (this.listLoading) return false;
+    else return true;
+  }
+
+  constructor(private service: VaccinationCenterService) { }
 
   ngOnInit(): void {
+    this.getResult();
   }
 
 }

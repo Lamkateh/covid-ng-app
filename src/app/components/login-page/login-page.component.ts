f479c639895ec42c6b1282dc51b3b8e6b771ec73
Gaëtan NOUSSE <gaetannousse@gmail.com>
2022-12-08 01:29:30 +0100

    Add login errors

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,6 +1,22 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { FormControl, FormGroupDirective, NgForm, Validators } from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
 import { Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
 
 @Component({
   selector: "app-login-page",
@@ -8,8 +24,11 @@ import { AuthService } from "../../services/auth.service";
   styleUrls: ["./login-page.component.scss"],
 })
 export class LoginPageComponent implements OnInit {
-  @Input() emailAddress?: string;
-  @Input() password?: string;
+  emailAddressFC = new FormControl("", [Validators.required, Validators.email,]);
+  passwordFC = new FormControl("", [Validators.required]);
+  matcher = new MyErrorStateMatcher();
+  loginLoading: boolean = false;
+  errorMessage: string | null = null;
   hide: boolean = true;
 
   constructor(private router: Router, private authService: AuthService) {
@@ -18,14 +37,29 @@ export class LoginPageComponent implements OnInit {
   ngOnInit(): void { }
 
   login() {
-    this.authService.signin(this.emailAddress, this.password).subscribe({
+    if (
+      this.emailAddressFC.invalid ||
+      this.passwordFC.invalid ||
+      this.emailAddressFC.value === "" ||
+      this.passwordFC.value === "" ||
+      this.emailAddressFC.value === null ||
+      this.passwordFC.value === null
+    ) {
+      return;
+    }
+    this.loginLoading = true;
+    this.errorMessage = null;
+    this.authService.signin(this.emailAddressFC.value, this.passwordFC.value).subscribe({
       next: (response: any) => {
-        this.authService.storeToken(this.emailAddress, this.password);
+        this.authService.storeToken(this.emailAddressFC.value, this.passwordFC.value);
         this.authService.setAuthUser(response.data);
+        this.loginLoading = false;
         this.router.navigate(["/centers"]);
       },
-      error: (error: any) => {
-        console.log(error);
+      error: (err: any) => {
+        this.loginLoading = false;
+        this.errorMessage = err.error.message;
+        console.log(err);
       },
     });
   }
